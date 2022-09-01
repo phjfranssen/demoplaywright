999082e0fdbda4a3bbfa2ab795ee184cb4fc3bfb
Peter Franssen <phj_franssen@hotmail.com>
2022-09-01 19:54:24 +0200

    Framework changes

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 1000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -32,7 +32,11 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', {  outputFolder: 'results', outputFile: 'test-results.html', open: 'never' }],
+    ['junit', {  outputFile: 'results/test-results.xml' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -41,7 +45,7 @@ const config = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
@@ -107,3 +111,5 @@ const config = {
 };
 
 module.exports = config;
+
+
